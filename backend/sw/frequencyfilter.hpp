cb6c6dc159e603ae8a2b87738265395f27d9cbc2
Stephan Widor <stephan@widor.de>
2022-06-23 22:02:12 +0200

    don't clear FrequencyFilter for zero frequency

--- backend/sw/frequencyfilter.hpp
+++ backend/sw/frequencyfilter.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "sw/basics/math.hpp"
+#include "sw/signals.hpp"
 #include <concepts>
 #include <vector>
 
@@ -15,25 +16,22 @@ public:
     {
         assert(averagingTime >= math::zero<F>);
 
-        if (frequency <= math::zero<F>)
-        {
-            m_buffer.clear();
-            return math::zero<F>;
-        }
-
+        const auto size = std::max(math::one<size_t>, static_cast<size_t>(std::round(averagingTime / sampleTime)));
         m_buffer.push_back(frequency);
-        m_size = std::max(math::one<size_t>, static_cast<size_t>(std::round(averagingTime / sampleTime)));
+        if (size < m_buffer.size())
+            m_buffer.erase(m_buffer.begin(), m_buffer.begin() + static_cast<int>(m_buffer.size() - size));
 
-        if (m_size < m_buffer.size())
-            m_buffer.erase(m_buffer.begin(), m_buffer.begin() + static_cast<int>(m_buffer.size() - m_size));
+        const auto average =
+          geometricAverage<F>(m_buffer | std::views::filter([](const auto f) { return f != math::zero<F>; }));
+        if (average > math::one<F>)
+            return average;
 
-        return geometricAverage<F>(m_buffer);
+        return math::zero<F>;
     }
 
     void clearBuffer() { m_buffer.clear(); }
 
 private:
-    size_t m_size{0u};
     std::vector<F> m_buffer;
 };
 

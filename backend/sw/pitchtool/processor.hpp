68da7a8a386fa69d322d4f272bfe423893d31ee5
Stephan Widor <stephan@widor.de>
2022-10-02 10:14:12 +0200

    bit of refactoring pitchtool::processor

--- backend/sw/pitchtool/processor.hpp
+++ backend/sw/pitchtool/processor.hpp
@@ -47,129 +47,41 @@ public:
         assert(std::ranges::ssize(signal) == stepSize);
         assert(std::ranges::ssize(o_signal) == stepSize);
 
-        const auto alignFormants = [&](const std::vector<SpectrumValue<F>> &spectrum,
-                                       std::vector<SpectrumValue<F>> &io_spectrumToAlign) {
-            envelopeAlignmentFactors<F>(gains<F>(spectrum), gains<F>(io_spectrumToAlign), m_envelopeAlignmentFactors);
-            std::transform(io_spectrumToAlign.begin(), io_spectrumToAlign.end(), m_envelopeAlignmentFactors.begin(),
-                           io_spectrumToAlign.begin(), [](const auto &spectrumValue, const F factor) {
-                               return SpectrumValue{spectrumValue.frequency, factor * spectrumValue.gain};
-                           });
-        };
-
-        const auto filterSpectrum = [&](ChannelState<F> &io_state) {
-            io_state.spectrum.apply([&](std::vector<SpectrumValue<F>> &o_buffer) {
-                o_buffer.clear();
-                std::copy_if(io_state.binSpectrum.begin() + 1, io_state.binSpectrum.end(), std::back_inserter(o_buffer),
-                             [zeroGainThresholdLinear = dBToFactor(static_cast<F>(-60))](const auto &value) {
-                                 return value.gain > zeroGainThresholdLinear;
-                             });
-                identifyFrequencies(o_buffer);
-            });
-        };
-
-        const auto tuningFactor = [&](const tuning::Type &type, TuningNoteEnvelope<F> &tuningEnvelope) {
-            const auto autoTuneToNote = [&](const tuning::AutoTune &autoTune) {
-                return autoTune.midiNoteNumber >= 0 ?
-                         fromMidi(autoTune.midiNoteNumber) :
-                         toNote(m_inputState.fundamentalFrequency.load(), tuningParameters.standardPitch);
-            };
-            const auto midiTuneToNote = [&](const tuning::MidiTune &midiTune) {
-                return midiTune.midiNoteNumber >= 0 ? fromMidi(midiTune.midiNoteNumber) : Note{Note::Name::Invalid, 0};
-            };
-
-            const auto noteFactor = [&](const Note &note, const float deviation = math::zero<F>) {
-                const auto envelopeFactor = tuningEnvelope.process(note, tuningParameters.attackTime, timeDiff);
-
-                if (m_inputState.fundamentalFrequency <= math::zero<F>)
-                    return math::one<F>;
-
-                const auto noteFrequency = toFrequency<F>(note, tuningParameters.standardPitch, deviation);
-
-                const auto tunedFrequency =
-                  std::pow(static_cast<F>(2), envelopeFactor * std::log2(m_inputState.fundamentalFrequency) +
-                                                (math::one<F> - envelopeFactor) * std::log2(noteFrequency));
-
-                return tunedFrequency / m_inputState.fundamentalFrequency;
-            };
-
-            return std::visit(overloaded{[](std::monostate) { return math::one<F>; },
-                                         [&](const tuning::AutoTune autoTune) {
-                                             return noteFactor(autoTuneToNote(autoTune),
-                                                               midiPitchBendToSemitones<F>(autoTune.pitchBend));
-                                         },
-                                         [&](const tuning::MidiTune midiTune) {
-                                             return noteFactor(midiTuneToNote(midiTune),
-                                                               midiPitchBendToSemitones<F>(midiTune.pitchBend));
-                                         }},
-                              type);
-        };
-
-        const auto processChannel = [&](const ChannelParameters<F> &parameters, ChannelState<F> &io_state) {
-            if (math::isZero(parameters.mixGain))
-            {
-                io_state.clear();
-                return;
-            }
-
-            const auto pitchFactor =
-              tuningFactor(parameters.tuningType, io_state.tuningEnvelope) * semitonesToFactor(parameters.pitchShift);
-
-            io_state.fundamentalFrequency = pitchFactor * m_inputState.fundamentalFrequency;
-
-            dft::shiftPitch<F>(pitchFactor, m_inputState.binSpectrum, io_state.binSpectrum);
-
-            const auto formantsFactor = semitonesToFactor(parameters.formantsShift);
-            if (!math::equal(pitchFactor, formantsFactor))
-            {
-                if (math::equal(formantsFactor, math::one<F>))
-                    alignFormants(m_inputState.binSpectrum, io_state.binSpectrum);
-                else
-                {
-                    dft::shiftPitch<F>(formantsFactor, m_inputState.binSpectrum, m_formantsSpectrum);
-                    alignFormants(m_formantsSpectrum, io_state.binSpectrum);
-                }
-            }
-
-            filterSpectrum(io_state);
-
-            shiftPhases<F>(io_state.phases, frequencies<F>(io_state.binSpectrum), timeDiff, io_state.phases);
-
-            dft::toBinCoefficients<F>(io_state.binSpectrum, io_state.phases, m_coefficients);
-
-            m_fft.transform_inverse(m_coefficients, m_processingSignal, false);
-
-            std::transform(m_processingSignal.begin(), m_processingSignal.end(), m_signalWindow.begin(),
-                           m_processingSignal.begin(),
-                           [factor = static_cast<F>(0.6)](const auto s, const auto w) { return factor * s * w; });
-
-            containers::ringPush(io_state.accumulator, math::zero<F>, stepSize);
-            std::transform(m_processingSignal.begin(), m_processingSignal.end(), io_state.accumulator.begin(),
-                           io_state.accumulator.begin(), std::plus());
-
-            std::transform(io_state.accumulator.begin(), io_state.accumulator.begin() + stepSize, o_signal.begin(),
-                           o_signal.begin(),
-                           [mixGain = parameters.mixGain](const auto stateSample, const auto outSample) {
-                               return mixGain * stateSample + outSample;
-                           });
-        };
-
         containers::ringPush(m_inputState.accumulator, signal);
-        std::transform(m_inputState.accumulator.begin(), m_inputState.accumulator.begin() + stepSize, o_signal.begin(),
-                       [dryMixGain](const auto sample) { return dryMixGain * sample; });
 
         std::transform(m_signalWindow.begin(), m_signalWindow.end(),
                        m_inputState.accumulator.end() - static_cast<int>(m_fftLength), m_processingSignal.begin(),
                        std::multiplies());
+
         m_fft.transform(m_processingSignal, m_coefficients, false);
+
         dft::toSpectrumByPhase<F>(sampleRate, timeDiff, m_inputState.phases, m_coefficients, m_inputState.binSpectrum,
                                   m_inputState.phases);
+
         filterSpectrum(m_inputState);
+
         m_inputState.fundamentalFrequency =
           m_frequencyFilter.process(findFundamental<F>(m_inputState.spectrum.inBuffer()).frequency, timeDiff,
                                     tuningParameters.averagingTime, tuningParameters.holdTime);
 
         for (auto i = 0u; i < NumChannels; ++i)
-            processChannel(channelParameters[i], m_channelStates[i]);
+            processChannel(channelParameters[i], tuningParameters, timeDiff, stepSize, m_channelStates[i]);
+
+        std::transform(m_inputState.accumulator.begin(), m_inputState.accumulator.begin() + stepSize, o_signal.begin(),
+                       [dryMixGain](const auto sample) { return dryMixGain * sample; });
+        for (auto i = 0u; i < NumChannels; ++i)
+        {
+            auto &channelState = m_channelStates[i];
+            const auto mixGain = channelParameters[i].mixGain;
+            if (!math::isZero(mixGain))
+            {
+                std::transform(channelState.accumulator.begin(), channelState.accumulator.begin() + stepSize,
+                               o_signal.begin(), o_signal.begin(),
+                               [mixGain](const auto stateSample, const auto outSample) {
+                                   return mixGain * stateSample + outSample;
+                               });
+            }
+        }
     }
 
     void processByPassed(ranges::TypedInputRange<F> auto &&signal, ranges::TypedRange<F> auto &&o_signal)
@@ -208,6 +120,111 @@ public:
     F outFundamentalFrequency(const size_t channel) const { return m_channelStates[channel].fundamentalFrequency; }
 
 private:
+    void filterSpectrum(ChannelState<F> &io_state)    // swdebug: binSpectrum and spectrum as direct params
+    {
+        io_state.spectrum.apply([&](std::vector<SpectrumValue<F>> &o_buffer) {
+            o_buffer.clear();
+            std::copy_if(io_state.binSpectrum.begin() + 1, io_state.binSpectrum.end(), std::back_inserter(o_buffer),
+                         [zeroGainThresholdLinear = dBToFactor(static_cast<F>(-60))](const auto &value) {
+                             return value.gain > zeroGainThresholdLinear;
+                         });
+            identifyFrequencies(o_buffer);
+        });
+    }
+
+    void processChannel(const ChannelParameters<F> &parameters, const TuningParameters<F> &tuningParameters,
+                        const F timeDiff, const int stepSize, ChannelState<F> &io_state)
+    {
+        if (math::isZero(parameters.mixGain))
+        {
+            io_state.clear();
+            return;
+        }
+
+        const auto pitchFactor =
+          tuningFactor(tuningParameters, parameters.tuningType, io_state.tuningEnvelope, timeDiff) *
+          semitonesToFactor(parameters.pitchShift);
+
+        io_state.fundamentalFrequency = pitchFactor * m_inputState.fundamentalFrequency;
+
+        dft::shiftPitch<F>(pitchFactor, m_inputState.binSpectrum, io_state.binSpectrum);
+
+        const auto formantsFactor = semitonesToFactor(parameters.formantsShift);
+        if (!math::equal(pitchFactor, formantsFactor))
+        {
+            if (math::equal(formantsFactor, math::one<F>))
+                alignFormants(m_inputState.binSpectrum, io_state.binSpectrum);
+            else
+            {
+                dft::shiftPitch<F>(formantsFactor, m_inputState.binSpectrum, m_formantsSpectrum);
+                alignFormants(m_formantsSpectrum, io_state.binSpectrum);
+            }
+        }
+
+        filterSpectrum(io_state);
+
+        shiftPhases<F>(io_state.phases, frequencies<F>(io_state.binSpectrum), timeDiff, io_state.phases);
+
+        dft::toBinCoefficients<F>(io_state.binSpectrum, io_state.phases, m_coefficients);
+
+        m_fft.transform_inverse(m_coefficients, m_processingSignal, false);
+
+        std::transform(m_processingSignal.begin(), m_processingSignal.end(), m_signalWindow.begin(),
+                       m_processingSignal.begin(),
+                       [factor = static_cast<F>(0.6)](const auto s, const auto w) { return factor * s * w; });
+
+        containers::ringPush(io_state.accumulator, math::zero<F>, stepSize);
+        std::transform(m_processingSignal.begin(), m_processingSignal.end(), io_state.accumulator.begin(),
+                       io_state.accumulator.begin(), std::plus());
+    };
+
+    F tuningFactor(const TuningParameters<F> &tuningParameters, const tuning::Type &type,
+                   TuningNoteEnvelope<F> &tuningEnvelope, const F timeDiff)
+    {
+        const auto autoTuneToNote = [&](const tuning::AutoTune &autoTune) {
+            return autoTune.midiNoteNumber >= 0 ?
+                     fromMidi(autoTune.midiNoteNumber) :
+                     toNote(m_inputState.fundamentalFrequency.load(), tuningParameters.standardPitch);
+        };
+        const auto midiTuneToNote = [&](const tuning::MidiTune &midiTune) {
+            return midiTune.midiNoteNumber >= 0 ? fromMidi(midiTune.midiNoteNumber) : Note{Note::Name::Invalid, 0};
+        };
+
+        const auto noteFactor = [&](const Note &note, const float deviation = math::zero<F>) {
+            const auto envelopeFactor = tuningEnvelope.process(note, tuningParameters.attackTime, timeDiff);
+
+            if (m_inputState.fundamentalFrequency <= math::zero<F>)
+                return math::one<F>;
+
+            const auto noteFrequency = toFrequency<F>(note, tuningParameters.standardPitch, deviation);
+
+            const auto tunedFrequency =
+              std::pow(static_cast<F>(2), envelopeFactor * std::log2(m_inputState.fundamentalFrequency) +
+                                            (math::one<F> - envelopeFactor) * std::log2(noteFrequency));
+
+            return tunedFrequency / m_inputState.fundamentalFrequency;
+        };
+
+        return std::visit(
+          overloaded{[](std::monostate) { return math::one<F>; },
+                     [&](const tuning::AutoTune autoTune) {
+                         return noteFactor(autoTuneToNote(autoTune), midiPitchBendToSemitones<F>(autoTune.pitchBend));
+                     },
+                     [&](const tuning::MidiTune midiTune) {
+                         return noteFactor(midiTuneToNote(midiTune), midiPitchBendToSemitones<F>(midiTune.pitchBend));
+                     }},
+          type);
+    };
+
+    void alignFormants(const std::vector<SpectrumValue<F>> &spectrum, std::vector<SpectrumValue<F>> &io_spectrumToAlign)
+    {
+        envelopeAlignmentFactors<F>(gains<F>(spectrum), gains<F>(io_spectrumToAlign), m_envelopeAlignmentFactors);
+        std::transform(io_spectrumToAlign.begin(), io_spectrumToAlign.end(), m_envelopeAlignmentFactors.begin(),
+                       io_spectrumToAlign.begin(), [](const auto &spectrumValue, const F factor) {
+                           return SpectrumValue{spectrumValue.frequency, factor * spectrumValue.gain};
+                       });
+    }
+
     size_t m_fftLength{0u};
     size_t m_overSampling{0u};
     sw::dft::FFT<F> m_fft;
